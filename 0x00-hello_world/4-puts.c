af913689f0b0db978052b0b45f1b2a056bcf4736
MustafaAdil98 <mostafaadillaljak@gmail.com>
2024-08-31 14:38:59 -0700

    modified puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
  * puzzle, followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-} 
+	return (0);
+}
