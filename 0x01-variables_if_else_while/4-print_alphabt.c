06a41dcc836ab8c5a15c1bf53df3fa469f760f14
MustafaAdil98 <mostafaadillaljak@gmail.com>
2024-09-01 07:14:45 -0700

    4-print_alphabt.c task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "program that prints the alphabet in lowercase, and then in
+ * uppercase, followed by a new line."
+ *
+ * Return: Always return (0).
+ */
+
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if(lowerCase == 'e' || lowerCase == 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+}
+return (0);
+}
+
