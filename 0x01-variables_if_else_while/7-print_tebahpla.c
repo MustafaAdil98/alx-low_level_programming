a0c1ac09195c7b8a3f3cebb5d67e30c092a61406
MustafaAdil98 <mostafaadillaljak@gmail.com>
2024-09-01 08:15:09 -0700

    7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+
+
+/**
+ * main - Entrypoint
+ *
+ * Description: "program that prints the lowercase alphabet in reverse,
+ * followed by a new line."
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+int lowerCase = 122;
+while (lowerCase > 66)
+{
+putchar(lowerCase);
+lowerCase -= 1;
+}
+putchar('\n');
+return (0);
+}
