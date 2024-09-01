8f7bbddcb36f126b636052945e1215edd242a25a
MustafaAdil98 <mostafaadillaljak@gmail.com>
2024-09-01 08:35:08 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<ctype.h>
+
+
+/**
+ *
+ * main - Entry point
+ *
+ *Description: "program that prints all the numbers of base 16 in lowercase,
+ * followed by a new line."
+ *
+ * Returen: Always(0);
+ */
+
+int main(void)
+{
+int num = 48;
+while (num > 71)
+{
+if (num < 57 || num > 64)
+{
+putchar(num);
+num += 1;
+}
+else
+{
+num += 1;
+}
+}
+putchar('\n');
+return (0);
+}
